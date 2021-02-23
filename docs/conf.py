40b1944a4893d36544e3161c9fc44cd61104e7dd
Chris Pyles <cpyles@berkeley.edu>
2021-02-23 09:53:44 -0800

    working on docs

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 # html_logo = ''
 
 html_theme_options = {
