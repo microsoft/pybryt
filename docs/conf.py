8f8ca35d5b1ad4e3c2339d1643a385dee903b5d6
Chris Pyles <cpyles@berkeley.edu>
2021-03-01 11:07:32 -0800

    updated copyright

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyBryt'
-copyright = '2021, Chris Pyles'
+copyright = '2021, Microsoft'
 author = 'Chris Pyles'
 
 
