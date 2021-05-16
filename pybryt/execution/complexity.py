8215cc9755a6fa269df51c5366323d5e053eaf55
Chris Pyles <cpyles@berkeley.edu>
2021-05-15 18:03:02 -0700

    changed from decorator so that errors can be handled

--- pybryt/execution/complexity.py
+++ pybryt/execution/complexity.py
@@ -17,33 +17,46 @@ class TimeComplexityResult:
         self.stop = stop
 
 
-@contextmanager
-def check_time_complexity(name: str, n: Union[int, float, Sized]):
+class check_time_complexity:
     """
     """
-    global _TRACKING_DISABLED
-    if isinstance(n, float):
-        n = int(n)
-    if isinstance(n, Sized):
-        n = len(n)
-    if not isinstance(n, int):
-        try:
+
+    def __init__(self, name: str, n: Union[int, float, Sized]):
+        if isinstance(n, float):
             n = int(n)
-        except:
-            raise TypeError(f"n has invalid type {type(n)}")
+        if isinstance(n, Sized):
+            n = len(n)
+        if not isinstance(n, int):
+            try:
+                n = int(n)
+            except:
+                raise TypeError(f"n has invalid type {type(n)}")
+  
+        self._name = name
+        self._n = n
+        self._curr_steps = None
+        self._observed, self._counter = None, None
 
-    from . import _COLLECTOR_RET
-    curr_steps = None
-    if _COLLECTOR_RET is not None:
-        observed, counter, _ = _COLLECTOR_RET
-        curr_steps = counter[0]
+    def __enter__(self):
+        global _TRACKING_DISABLED
 
-    _TRACKING_DISABLED = True
+        from . import _COLLECTOR_RET
+        if _COLLECTOR_RET is not None:
+            self._observed, self._counter, _ = _COLLECTOR_RET
+            self._curr_steps = self._counter[0]
 
-    yield
+        _TRACKING_DISABLED = True
 
-    _TRACKING_DISABLED = False
+    def __exit__(self, exc_type, exc_value, traceback):
+        global _TRACKING_DISABLED
 
-    if curr_steps is not None:
-        end_steps = counter[0]
-        observed.append((TimeComplexityResult(name, n, curr_steps, end_steps), end_steps))
+        _TRACKING_DISABLED = False
+
+        if self._curr_steps is not None:
+            end_steps = self._counter[0]
+            self._observed.append((
+                TimeComplexityResult(self._name, self._n, self._curr_steps, end_steps), 
+                end_steps,
+            ))
+
+        return False
