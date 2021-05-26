06ef78a50d825c37da042fc14e50b6a5e77bacb8
Chris Pyles <cpyles@berkeley.edu>
2021-05-26 15:53:14 -0400

    documentation update, report output

--- pybryt/cli.py
+++ pybryt/cli.py
@@ -4,11 +4,14 @@ import os
 import json
 import click
 
-from . import generate_student_impls, ReferenceImplementation, StudentImplementation
+from . import (
+    generate_report, generate_student_impls, ReferenceImplementation, StudentImplementation, __version__
+)
 from .utils import get_stem
 
 
 @click.group()
+@click.version_option(__version__)
 def click_cli():
     """
     A command-line interface for PyBryt. See https://microsoft.github.io/pybryt for more information.
@@ -16,15 +19,17 @@ def click_cli():
     pass
 
 
-@click_cli.command()
+@click_cli.command("compile")
 @click.option("-d", "--dest", default=None, type=click.Path(dir_okay=False), 
               help="Path at which to write the pickled reference implementation")
 @click.option("-n", "--name", default=None, type=click.STRING, 
               help="Optional name for the reference implementation")
 @click.argument("src", type=click.Path(exists=True, dir_okay=False))
-def compile(src, dest, name):
+def compile_reference(src, dest, name):
     """
     Compile the reference implementation SRC.
+
+    SRC must be a path to a Jupyter Notebook file which defines a reference implementation.
     """
     ref = ReferenceImplementation.compile(src, name=name)
     ref.dump(dest)
@@ -38,13 +43,18 @@ def compile(src, dest, name):
 @click.option("-o", "--output", default=None, type=click.Path(dir_okay=False), 
               help="Path at which to write the results of the check")
 @click.option("-t", "--type", "output_type", default="pickle", show_default=True, 
-              type=click.Choice(["pickle", "json"]), help="Type of output file to write")
+              type=click.Choice(["pickle", "json", "report"]), help="Type of output to write")
 @click.argument("ref", type=click.Path(exists=True, dir_okay=False))
 @click.argument("stu", type=click.Path(exists=True, dir_okay=False))
 def check(ref, stu, name, output_nb, output, output_type):
     """
-    Runs the student notebook STU against the reference implementation REF. REF can be a path to a
-    pickled reference implementation or to a notebook to be compiled on-the-fly.
+    Run a student submission against a reference implementation.
+
+    REF can be a path to a pickled reference implementation or to a notebook to be compiled on-the-fly.
+    STU can be a path to a pickled student implementation or to a notebook to be executed.
+
+    If TYPE is "pickle" or "json", the output is a file. If TYPE is "report", a report is echoed to
+    the console.
     """
     if os.path.splitext(ref)[1] == ".ipynb":
         ref = ReferenceImplementation.compile(ref)
@@ -57,7 +67,14 @@ def check(ref, stu, name, output_nb, output, output_type):
         except:
             raise RuntimeError(f"Could not load the reference implementation {ref}")
 
-    stu = StudentImplementation(stu, output=output_nb)
+    if os.path.splitext(stu)[1] == ".ipynb":
+        stu = StudentImplementation(stu, output=output_nb)
+    else:
+        try:
+            stu = StudentImplementation.load(stu)
+        except:
+            raise RuntimeError(f"Could not load the student implementation {stu}")
+
     res = stu.check(ref)
 
     if output_type == "pickle":
@@ -68,6 +85,9 @@ def check(ref, stu, name, output_nb, output, output_type):
         d = res.to_dict()
         with open(output, "w+") as f:
             json.dump(d, f, indent=2)
+    elif output_type == "report":
+        report = generate_report(res)
+        click.echo(report)
 
 
 @click_cli.command()
@@ -78,6 +98,11 @@ def check(ref, stu, name, output_nb, output, output_type):
 @click.argument("subm", nargs=-1, type=click.Path(exists=True, dir_okay=False))
 def execute(subm, parallel, output):
     """
+    Execute student submissions to generate memory footprints.
+
+    Executes the student submission(s) SUBM at writes the pickled objects to some output file. If
+    OUTPUT is unspecified, this defaults to "./{SUBM.stem}.pkl" (e.g. for SUBM 
+    "submissions/subm01.ipynb", this is "./subm01.pkl").
     """
     if len(subm) == 0:
         raise ValueError("You must specify at least one notebook to execute")
