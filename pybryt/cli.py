82750b1f769db16980ef6e9b84758a47d88cd367
Chris Pyles <cpyles@berkeley.edu>
2021-05-25 23:53:21 -0400

    working on cli

--- pybryt/cli.py
+++ pybryt/cli.py
@@ -1,8 +1,10 @@
 """Command-line interface for PyBryt"""
 
+import os
+import json
 import click
 
-from . import ReferenceImplementation
+from . import ReferenceImplementation, StudentImplementation
 
 
 @click.group()
@@ -24,6 +26,43 @@ def compile(src, dest, name):
     """
     ref = ReferenceImplementation.compile(src, name=name)
     ref.dump(dest)
+
+
+@click_cli.command()
+@click.option("-n", "--name", default=None, type=click.STRING, 
+              help="Optional name for the reference implementation")
+@click.option("--output-nb", default=None, type=click.Path(dir_okay=False))
+@click.option("-o", "--output", default=None, type=click.Path(dir_okay=False))
+@click.option("-t", "--type", "output_type", default="pickle", show_default=True, type=click.Choice(["pickle", "json"]))
+@click.argument("ref", type=click.Path(exists=True, dir_okay=False))
+@click.argument("stu", type=click.Path(exists=True, dir_okay=False))
+def check(ref, stu, name, output_nb, output, output_type):
+    """
+    Runs the student notebook STU against the reference implementation REF. REF can be a path to a
+    pickled reference implementation or to a notebook to be compiled on-the-fly.
+    """
+    if os.path.splitext(ref)[1] == ".ipynb":
+        ref = ReferenceImplementation.compile(ref)
+        if not isinstance(ref, ReferenceImplementation):
+            raise RuntimeError("On-the-fly reference compilation must result in a single reference "
+                               "implementation")
+    else:
+        try:
+            ref = ReferenceImplementation.load(ref)
+        except:
+            raise RuntimeError(f"Could not load the reference implementation {ref}")
+
+    stu = StudentImplementation(stu, output=output_nb)
+    res = stu.check(ref)
+
+    if output_type == "pickle":
+        res.dump(output)
+    elif output_type == "json":
+        if output is None:
+            output = os.path.splitext(res._default_dump_dest)[0] + ".json"
+        d = res.to_dict()
+        with open(output, "w+") as f:
+            json.dump(d, f, indent=2)
 
 
 def cli(*args, **kwargs):
