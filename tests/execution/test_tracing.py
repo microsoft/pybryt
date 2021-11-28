963ae33e779c864cea4373e87e2f7106e273f3dd
Chris Pyles <cpyles@berkeley.edu>
2021-11-27 21:05:08 -0800

    tests for preprocessor and trace fn

--- tests/execution/test_tracing.py
+++ tests/execution/test_tracing.py
@@ -146,6 +146,16 @@ def test_trace_function():
     assert np.allclose(footprint.values[0][0], arr)
     assert footprint.values[0][1] == 1
 
+    with mock.patch.object(footprint, "add_imports") as mocked_add_imports:
+        class Foo:
+            pass
+
+        cir(frame, "return", Foo)
+        mocked_add_imports.assert_called_with(Foo.__module__.split(".")[0])
+
+        cir(frame, "return", np)
+        mocked_add_imports.assert_called_with(np.__name__)
+
 
 def test_tracing_control():
     """
